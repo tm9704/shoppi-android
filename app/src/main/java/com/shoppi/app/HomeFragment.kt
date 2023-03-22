83ed7d7d0039e3f403e2982b893aa6ebc258bf07
Min Hyung Lee <tm9704@korea.ac.kr>
2023-03-22 22:44:18 +0900

    Json asset 불러오기

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.shoppi.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -25,5 +26,9 @@ class HomeFragment: Fragment() {
         button.setOnClickListener {
             findNavController().navigate(R.id.action_home_to_product_detail)
         }
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeData ?: "")
     }
 }
