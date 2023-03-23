58da9ee0cfb946632d4afd5ff56e1eb87552ff05
tm9704 <tm9704@korea.ac.kr>
2023-03-23 19:36:27 +0900

    ViewPager - Adapter 구현

---
+++ app/src/main/java/com/shoppi/app/Banner.kt
@@ -0,0 +1,22 @@
+package com.shoppi.app
+
+data class Banner(
+    val backgroundImageUrl: String,
+    val badge: BannerBadge,
+    val label: String,
+    val productDetail: ProductDetail
+)
+
+data class BannerBadge(
+    val label: String,
+    val backgroundColor: String
+)
+
+data class ProductDetail(
+    val brandName: String,
+    val label: String,
+    val discountRate: Int,
+    val price: Int,
+    val thumbnailImageUrl: String,
+    val productId: String
+)
