4aea37bbbd8a343238c866485bfb96937ae428ff
tm9704 <tm9704@korea.ac.kr>
2023-03-23 10:18:32 +0900

    Appbar 내부 TextView, ImageView 배치 & JSON 데이터 활용 & Glide 라이브러리 활용

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.json.JSONObject
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,27 +25,21 @@ class HomeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_product_detail)
-        }
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
 
         val assetLoader = AssetLoader()
         val homeData = assetLoader.getJsonString(requireContext(), "home.json")
 
-        if(!homeData.isNullOrEmpty()){
+        if (!homeData.isNullOrEmpty()) {
             val jsonObject = JSONObject(homeData)
             val title = jsonObject.getJSONObject("title")
             val text = title.getString("text")
             val iconUrl = title.getString("icon_url")
-            val titleValue = Title(text, iconUrl)
-            titleValue.text
-//
-//            val topBanners = jsonObject.getJSONArray("top_banners")
-//            val firstBanner = topBanners.getJSONObject(0)
-//            firstBanner.getString("label")
-//            val productDetail = firstBanner.getJSONObject("product_detail")
-//            val price = productDetail.getInt("price")
+            toolbarTitle.text = text
+            GlideApp.with(this)
+                .load(iconUrl)
+                .into(toolbarIcon)
         }
     }
 }
