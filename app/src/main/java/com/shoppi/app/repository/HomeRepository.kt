02471012b52b25c9978aec7c1cad3da2749047ea
tm9704 <tm9704@korea.ac.kr>
2023-03-24 15:49:25 +0900

    Data Layer

---
+++ app/src/main/java/com/shoppi/app/repository/HomeRepository.kt
@@ -0,0 +1,10 @@
+package com.shoppi.app.repository
+
+import com.shoppi.app.model.HomeData
+
+class HomeRepository(private val assetDataSource: HomeAssetDataSource) {
+
+    fun getHomeData(): HomeData? {
+        return assetDataSource.getHomeData()
+    }
+}
