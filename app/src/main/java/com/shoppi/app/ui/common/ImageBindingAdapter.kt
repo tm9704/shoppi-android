c5a2dcd9ae225e0046efc2fda0ed86fd8f122758
tm9704 <tm9704@korea.ac.kr>
2023-03-29 14:14:46 +0900

    세부 카테고리 화면 구현 - 중접 RecyclerView layout구현

--- app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapter.kt
+++ app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapter.kt
@@ -4,11 +4,22 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.shoppi.app.GlideApp
 
+
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, imageUrl: String?) {
     if(!imageUrl.isNullOrEmpty()){
         GlideApp.with(view)
             .load(imageUrl)
+            .into(view)
+    }
+}
+
+@BindingAdapter("circleImageUrl")
+fun loadCircleImage(view: ImageView, imageUrl: String?) {
+    if(!imageUrl.isNullOrEmpty()){
+        GlideApp.with(view)
+            .load(imageUrl)
+            .circleCrop()
             .into(view)
     }
 }
