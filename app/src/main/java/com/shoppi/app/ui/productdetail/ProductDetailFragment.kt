f7b3f1c32699f49696896c990aef71ac73e44f4b
tm9704 <tm9704@korea.ac.kr>
2023-04-01 20:40:43 +0900

    장바구니 Data Layer 구현 (완)

--- app/src/main/java/com/shoppi/app/ui/productdetail/ProductDetailFragment.kt
+++ app/src/main/java/com/shoppi/app/ui/productdetail/ProductDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.shoppi.app.ui.productdetail
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,14 +9,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.load.model.stream.QMediaStoreUriLoader.InputStreamFactory
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.shoppi.app.R
 import com.shoppi.app.common.KEY_PRODUCT_ID
 import com.shoppi.app.databinding.FragmentProductDetailBinding
+import com.shoppi.app.ui.common.EventObserver
 import com.shoppi.app.ui.common.ViewModelFactory
 
-class ProductDetailFragment: Fragment() {
+class ProductDetailFragment : Fragment() {
 
-    private val viewModel: ProductDetailViewModel by viewModels {ViewModelFactory(requireContext())}
+    private val viewModel: ProductDetailViewModel by viewModels { ViewModelFactory(requireContext()) }
     private lateinit var binding: FragmentProductDetailBinding
 
     override fun onCreateView(
@@ -27,23 +30,37 @@ class ProductDetailFragment: Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
         setNavigation()
-        requireArguments().getString(KEY_PRODUCT_ID)?.let{ productId ->
+        requireArguments().getString(KEY_PRODUCT_ID)?.let { productId ->
             setLayout(productId)
         }
+
+        setAddCart()
+    }
+
+    private fun setAddCart() {
+        viewModel.addCartEvent.observe(viewLifecycleOwner, EventObserver {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("장바구니에 상품이 담겼습니다.")
+                .setPositiveButton("확인") { dialog, which ->
+
+                }
+                .show()
+        })
     }
 
     private fun setNavigation() {
-        binding.toolbarProductDetail.setNavigationOnClickListener{
+        binding.toolbarProductDetail.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
     }
 
-    private fun setLayout(productId: String){
+    private fun setLayout(productId: String) {
         viewModel.loadProductDetail(productId)
         val descriptionAdapter = ProductDescriptionAdapter()
         binding.rvProductDetail.adapter = descriptionAdapter
